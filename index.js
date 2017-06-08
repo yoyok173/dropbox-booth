f011d7ebac31ba862e869560f7dde2e896b63136
Marcus Kirsch <briefkasten@marcuskirsch.de>
2017-06-08 21:37:39 +0200

    add basic auth for node

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fileUpload = require('express-fileupload')
 const uuid = require('uuid/v4')
 const Dropbox = require('dropbox')
 const path = require('path')
+const auth = require('basic-auth')
 
 dotenv.config()
 
@@ -19,6 +20,18 @@ app.use(fileUpload())
 app.use(express.static(process.cwd(), {
   maxage: 2592000000 // set max-cache to 30d
 }))
+
+app.use((req, res, next) => {
+  let credentials = auth(req)
+
+  if (!credentials || credentials.name !== process.env.USER || credentials.pass !== process.env.PASSWORD) {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="denied"')
+    res.send('Access denied')
+  } else {
+    next()
+  }
+})
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/index.html'))
@@ -69,7 +82,7 @@ channel.on('connection', (socket) => {
   })
 })
 
-const port = process.env.PORT || 8001
+const port = process.env.PORT || 5000
 server.listen(port, () => {
   console.log('Listening on ' + port)
 })
